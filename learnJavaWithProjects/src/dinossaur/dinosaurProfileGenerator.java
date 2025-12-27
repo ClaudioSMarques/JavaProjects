48230ba8f26d522f89e3ff18505e91833ae5b49d
Claudio Marques <claudio.smarques391@gmail.com>
2025-12-27 13:39:02 -0300

    Dinosaurs Profile Generator

---
+++ learnJavaWithProjects/src/dinossaur/dinosaurProfileGenerator.java
@@ -0,0 +1,17 @@
+package dinossaur;
+
+public class dinosaurProfileGenerator {
+	public static void main(String[] args) {
+		//Declare variables for dinosaur profile
+		String name = "Turbo";
+		int age = 6;
+		String species = "Stegosaurus";
+		String diet = "Herbivore";
+		int weight = 5000; // in kilograms
+		
+		//Print dinosaur profile:
+		System.out.println("Meet " + name + ", a " + age + " years-old " + species + ". As a " + diet + ", it has a robust weight of " + weight +" kilograms.");
+		
+	}
+
+}
